74b7aa731c8aed279e240e35eb13a097fa6e3d38
Richard Marko <srk@48.io>
2019-11-09 18:11:28 +0100

    tower: add unsafeStateAttrs
    Like state but with area attributes. E.g.:
    
    st <- unsafeStateAttrs "varName" [Section ".myLinkerSection"]
    
    Might not be initialized by startup routine thus unsafe.

--- tower/src/Ivory/Tower/Monitor.hs
+++ tower/src/Ivory/Tower/Monitor.hs
@@ -4,6 +4,7 @@ module Ivory.Tower.Monitor
   ( handler
   , state
   , stateInit
+  , unsafeStateAttrs
   , monitorModuleDef
   , Handler()
   , Monitor()
@@ -18,18 +19,33 @@ import Ivory.Language
 
 state :: (IvoryArea a, IvoryZero a)
       => String -> Monitor e (Ref 'Global a)
-state n = state' n Nothing
+state n = state' n Nothing []
 
 stateInit :: (IvoryArea a, IvoryZero a)
           => String -> Init a -> Monitor e (Ref 'Global a)
-stateInit n i = state' n (Just i)
+stateInit n i = state' n (Just i) []
+
+-- Accepts AreaAttribute for example Section "xy" to place
+-- this state to "xy" memory section according to linker script
+--
+-- This is used in combination with memory retention
+-- functions of microcontrollers, can be also used to
+-- put variables into special sections of MCU like battery
+-- backed ram or core coupled memory.
+--
+-- Marked as unsafe due to need for manually initializing
+-- such variables (if needed).
+unsafeStateAttrs :: (IvoryArea a, IvoryZero a)
+      => String -> [AreaAttribute] -> Monitor e (Ref 'Global a)
+unsafeStateAttrs n attrs = state' n Nothing attrs
 
 state' :: (IvoryArea a, IvoryZero a)
        => String
        -> Maybe (Init a)
+       -> [AreaAttribute]
        -> Monitor e (Ref 'Global a)
-state' n i = do
+state' n i attrs = do
   f <- freshname n
-  let a = area (showUnique f) i
+  let a  = setMemAreaAttributes attrs $ area (showUnique f) i
   monitorModuleDef $ defMemArea a
   return (addrOf a)
