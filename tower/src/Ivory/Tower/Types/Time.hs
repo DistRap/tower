3e2a72eb4bd8bc8101090a09d3100e1b6c6258ec
Richard Marko <srk@48.io>
2019-12-07 18:41:59 +0100

    tower: add Seconds type, seconds, minutes, hours, days constructors

--- tower/src/Ivory/Tower/Types/Time.hs
+++ tower/src/Ivory/Tower/Types/Time.hs
@@ -10,17 +10,24 @@ module Ivory.Tower.Types.Time
   ( Time
   , Microseconds(..)
   , Milliseconds(..)
+  , Seconds(..)
   , toMicroseconds
   , toMilliseconds
   , microseconds
   , us
   , ms
+  , seconds
+  , minutes
+  , hours
+  , days
 
   , ITime
   , fromIMicroseconds
   , fromIMilliseconds
+  , fromISeconds
   , toIMicroseconds
   , toIMilliseconds
+  , toISeconds
   , toITime
   , prettyTime
   ) where
@@ -55,6 +62,22 @@ instance Time Milliseconds where
 ms :: Integer -> Milliseconds
 ms = Milliseconds
 
+newtype Seconds = Seconds Integer deriving (Eq, Show, Ord)
+instance Time Seconds where
+  toMicroseconds (Seconds t) = t * 1000 * 1000
+
+seconds :: Integer -> Seconds
+seconds = Seconds
+
+minutes :: Integer -> Seconds
+minutes = Seconds . (*60)
+
+hours :: Integer -> Seconds
+hours = minutes . (*60)
+
+days :: Integer -> Seconds
+days = hours . (*24)
+
 newtype ITime = ITime Sint64
   deriving ( Num, IvoryType, IvoryVar, IvoryExpr, IvoryEq, IvoryOrd
            , IvoryIntegral, IvoryStore, IvoryInit, IvoryZeroVal, Bounded)
@@ -67,11 +90,17 @@ fromIMicroseconds = ITime . safeCast
 fromIMilliseconds :: (SafeCast a Sint64) => a -> ITime
 fromIMilliseconds = ITime . (*1000) . safeCast
 
+fromISeconds :: (SafeCast a Sint64) => a -> ITime
+fromISeconds = ITime . (*1000000) . safeCast
+
 toIMicroseconds :: ITime -> Sint64
 toIMicroseconds (ITime t) = t
 
 toIMilliseconds :: ITime -> Sint64
 toIMilliseconds (ITime t) = t `iDiv` 1000
+
+toISeconds :: ITime -> Sint64
+toISeconds (ITime t) = t `iDiv` 1000000
 
 toITime :: (Time a) => a -> ITime
 toITime t = fromIMicroseconds us'
@@ -79,12 +108,14 @@ toITime t = fromIMicroseconds us'
   us' :: Sint64
   us' = fromIntegral (toMicroseconds t)
 
-prettyTime :: Microseconds -> String
+prettyTime :: (Time a) => a -> String
 prettyTime m = t
   where
   us' = toMicroseconds m
   t  = case us' `mod` 1000 of
-    0 -> (show (us' `div` 1000)) ++ "ms"
+    0 -> case us' `mod` 1000000 of
+           0 -> (show (us' `div` 1000000)) ++ "s"
+           _ -> (show (us' `div` 1000)) ++ "ms"
     _ -> (show us') ++ "us"
 
 instance Pretty Microseconds where
